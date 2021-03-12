5527c592e9aeaa29e3ba8d1ab13f95503c3aaf1f
khuth <kathahuth@yahoo.de>
2021-03-12 12:03:54 +0100

    init backend

---
+++ backend/src/main/java/de/kathahuth/gymast/GymastApplication.java
@@ -0,0 +1,13 @@
+package de.kathahuth.gymast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymastApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/kathahuth/gymast/GymastApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kathahuth.gymast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
