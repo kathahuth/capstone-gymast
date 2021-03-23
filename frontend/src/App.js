31ba2463e0588b0e3ce8d8110be7e2395e7bf62b
khuth <kathahuth@yahoo.de>
2021-03-23 19:01:01 +0100

    remove unused imports in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import AppHeader from "./components/AppHeader";
 import DifficultyBoards from "./components/DifficultyBoards";
 import NavigationBar from "./components/NavigationBar";
 import PageLayout from "./components/PageLayout";
-import {useEffect, useState} from 'react'
-import {getDifficultyGroups} from "./services/MongoDbApiService";
 
 
 export default function App() {
