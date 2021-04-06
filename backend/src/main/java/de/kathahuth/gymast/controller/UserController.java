02fac8b0daf621efceafca7e746320331987b25c
khuth <kathahuth@yahoo.de>
2021-04-06 10:38:14 +0200

    refactor api url in user controller

--- backend/src/main/java/de/kathahuth/gymast/controller/UserController.java
+++ backend/src/main/java/de/kathahuth/gymast/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/user")
+@RequestMapping("api/gymnast")
 public class UserController {
 
     private final UserService userService;
