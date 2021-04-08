911c9a9d27bcad505ac8ca26cf30e42118987e6c
khuth <kathahuth@yahoo.de>
2021-04-08 17:21:29 +0200

    Add welcome page with markup and design

---
+++ frontend/src/components/button/Welcome.js
@@ -0,0 +1,10 @@
+import ArrowRight from "../../icon/ArrowRight";
+import LinkStyled from "../../form/LinkStyled";
+
+export default function GetStartedButton() {
+
+    return (
+        <LinkStyled to='/difficulty'>Get started <ArrowRight/></LinkStyled>
+    )
+
+}
---
+++ frontend/src/pages/WelcomePage.js
@@ -0,0 +1,25 @@
+import styled from "styled-components/macro";
+import WelcomeBoard from "../components/boards/WelcomeBoard";
+
+export default function WelcomePage() {
+
+    return (
+        <Wrapper>
+            <WelcomeBoard/>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+
+    height: 100vh;
+    background: url('BackgroundIndividualGymnast.jpg') fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin: 0;
+  
+  
+    
+    
+`
